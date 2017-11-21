574632b4a9a2892f930528ed82af6af586b6c8fc
hushi1991 <davi0279@stud.kea.dk>
2017-11-21 13:00:52 +0100

    Indextest side

--- src/main/java/dk/sjd/demo/Controller/HomeController.java
+++ src/main/java/dk/sjd/demo/Controller/HomeController.java
@@ -1,7 +1,18 @@
 package dk.sjd.demo.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
 public class HomeController {
 
     // Hej Silas
+
+    @RequestMapping(value = {"","/","index"}, method = RequestMethod.GET)
+    public String index() {
+        return "index";
+    }
 
 }
