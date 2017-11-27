e608fbb5ff96bf01df1c7546e8d6c58246551674
hushi1991 <davi0279@stud.kea.dk>
2017-11-27 12:26:29 +0100

    Employeerepo

---
+++ src/main/java/dk/sjd/demo/Model/Repositories/EmployeeRepository.java
@@ -0,0 +1,28 @@
+package dk.sjd.demo.Model.Repositories;
+
+import dk.sjd.demo.Model.Entities.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+
+@Repository
+public class EmployeeRepository implements IEmployeeRepository {
+
+    @Autowired
+    private JdbcTemplate jdbc;
+
+    @Override
+    public ArrayList<Employee> readAll() {
+        SqlRowSet sqlRowSet = jdbc.queryForRowSet("SELECT * FROM  employees");
+        ArrayList<Employee> employees = new ArrayList<>();
+
+        while(sqlRowSet.next()) {
+            employees.add(new Employee(sqlRowSet.getInt("id"), sqlRowSet.getString("name"), sqlRowSet.getInt("totalHours")));
+        }
+
+        return employees;
+    }
+}
